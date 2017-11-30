a056ea815ef38743eefdf23e770cdc5e547b9f0e
IntiDev <yeseniase@outlook.it>
2017-11-30 03:00:37 -0600

    Se agregan comentarios al js

--- js/app.js
+++ js/app.js
@@ -32,8 +32,8 @@ var flipped = 0;
 var pairs = 0;
 
 function rotation() {
-  // Validamos que la carta se pueda voltear
-  if(!this.classList.contains('disabled')) {
+  // Validamos que la carta se puedan voltear CASO PARA OCULTAR LAS CARTAS
+  if(!this.classList.contains('disabled')) {//Se usa como indicador para desabilitar efecto de rotacion
     if ( this.classList.contains('rotation-efect') ) {
       /*
       * Agregamos el atributo selected para interactuar con los estilos
@@ -41,10 +41,10 @@ function rotation() {
       */
       this.setAttribute("selected", !(this.getAttribute("selected") === "true"));
       this.classList.remove('rotation-efect');
-      flipped--;
+      flipped--; //se resta para las cartas que pasan de boca aariba a boca abajo
     }
     else {
-      // Limitamos las cartas volteadas a 2
+      // Limitamos las cartas volteadas a 2 (CASO PARA MOSTRAR CARTAS)
       if(flipped <= 2) {
         this.setAttribute("selected", !(this.getAttribute("selected") === "true"));
         this.classList.add('rotation-efect');
@@ -54,16 +54,18 @@ function rotation() {
           var flippedCards = document.getElementsByClassName('rotation-efect');
           var firstFlipped = flippedCards[0];
           var secondFlipped = flippedCards[1];
-          if(hasEqualClasses(firstFlipped, secondFlipped)) {
+          if(hasEqualClasses(firstFlipped, secondFlipped)) { //se llama a la funcion hasEqualClasses que de cumplir la concion regresa TRUE
             // Deshabilitamos las cartas
             firstFlipped.classList.add("disabled");
             secondFlipped.classList.add("disabled");
-            // Les quitamos la clase que usamos para validar
+            // Les quitamos la clase que usamos para rotar
             firstFlipped.classList.remove('rotation-efect');
             secondFlipped.classList.remove('rotation-efect');
-            flipped = 0;
+            flipped = 0; //reiniciamos el contador de cartas volteadas (PARA SOLO TRABAJAR CON PARES)
             // Validamos si ha ganado
-            if(++pairs == 4) {
+            // var x = i ++ primero asignas el valor de i y luego la incremetnas
+            // var x = ++i primero incrementas i y luego se asigna a x
+            if(++pairs == 4) {//realizamos 2 intrucciones en una misma linea (el incremento y la comparacion)
               // Agregamos una pausa para que se pueda ver la animacion
               setTimeout(function() {
                 var response = confirm("You win!\n Do u wanna play again?");
@@ -75,7 +77,7 @@ function rotation() {
               }, 1000);
             }
           } else {
-            // Agregamos una pausa para que se pueda ver la animacion
+            // Agregamos una pausa para que se pueda ver la animacion de la segunda carta que selecionamos
             setTimeout(function() {
               firstFlipped.setAttribute("selected", false);
               firstFlipped.classList.remove('rotation-efect');
@@ -96,16 +98,18 @@ function rotation() {
 * Si son iguales las deshabilitamos
 */
 function hasEqualClasses(firstElement, secondElement) {
-  var firstElementClasses = firstElement.classList.value.split(" ");
+  var firstElementClasses = firstElement.classList.value.split(" ");//devuelve un arreglo de strings con todas las clases
   var secondElementClasses = secondElement.classList.value.split(" ");
-  if(firstElementClasses.length == secondElementClasses.length) {
-    return firstElementClasses.every(function(value, iterator) { return value == secondElementClasses[iterator]})
+  if(firstElementClasses.length == secondElementClasses.length) { //validamos tamanio
+    return firstElementClasses.every(function(value, iterator) { //validamos que TODAS las clases sean iguales
+      return value == secondElementClasses[iterator]})//VALUE = todas las clases del 1er elemento
   } else {
     return false;
   }
 }
 
 function restart() {
+  pairs = 0;// para reiniciar el juego infinitamente
   for(var i = 0; i < arrayCardsSize; i++) {
     cards[i].classList.remove("disabled");
     cards[i].setAttribute("selected", false);
